29cfc79d72dc0959a7be7befb538f699349ee250
danielebercovici <daniele.bercovici@mail.mcgill.ca>
2018-04-11 22:16:56 -0400

    Add files via upload

--- A3/resource_request_simulator.c
+++ A3/resource_request_simulator.c
@@ -17,12 +17,12 @@ Simulates resource requests by processes
 */
 void request_simulator(int pr_id, int* request_vector){
 //call to get a request vector REQj
-//requests ij instances of each resource j where i is randomly selected as a value between 0 and amount of remaining resources
-
-for(int j =0; j<numResources;j++){
-    //rand() % (max_number + 1 - minimum_number) + minimum_number
-    request_vector[j]=rand()%(need[pr_id][j]+1);
-}
+    for(int j =0; j<numResources;j++){
+        //rand() % (max_number + 1 - minimum_number) + minimum_number
+        printf("[%d] NEED: %d \n",pr_id, need[pr_id][j]);
+        request_vector[j]=rand()%(need[pr_id][j]+1);
+        printf("[%d] REQUEST: %d \n",pr_id, request_vector[j]);
+    }
 }
 
 /*
@@ -31,12 +31,33 @@ Implementation of isSafe() as described in the slides
 int isSafe(){
     //initalize work
     int *work;
+    int isSafe = 1;
+
     work = malloc(numResources * sizeof(int));
     for(int j =0;j<numResources;j++){
-
+        work[j]=avail[j];
     }
     bool *finish;
-    int isSafe = 0;
+    finish = malloc(numProcesses * sizeof(int));
+    for(int i=0;i<numProcesses;i++){
+        finish[i]=false;
+    }
+
+    for(int i = 0; i<numProcesses;i++){
+        for(int j =0; j<numResources;j++){
+            if(finish[i]==false && need[i][j]<=work[j]){
+                work[j]=work[j]-hold[i][j];
+                finish[i]=true;
+            }
+        }
+    } 
+   
+    for(int i=0;i<numProcesses;i++){
+        if(finish[i] == true){}
+        else{
+            isSafe=0; //not safe
+        }  
+    }
     return isSafe;
 
 }
@@ -58,8 +79,8 @@ for(int i = 0; i<numProcesses;i++){
             for(int j=0;j<numResources;j++){
                 if(request_vector[j]>avail[j]){
                     //must wait --------------------------------------------- return 0;??
-                    
-                    bankers_algorithm(pr_id,request_vector);
+                    return 0;
+                    //bankers_algorithm(pr_id,request_vector);
                 }
             }
             //provisional allocation
@@ -70,14 +91,36 @@ for(int i = 0; i<numProcesses;i++){
                 for(int j =0; j<numResources;j++){
                     hold[i][j]=hold[i][j]+request_vector[j];
                     need[i][j]=need[i][j]-request_vector[j];
+                    printf("process [%d], resource [%d] new need %d \n",i,j, need[i][j]);
                 }
             }
             if(isSafe()){ //grant
+                printf("GRANTED \n");
+                sleep(3); //---------------- is this here???, WHERE DO WE RELIQUISH RESOURCES?
+                //reliquish??
+                for(int i = 0; i<numProcesses;i++){
+                    for(int j =0; j<numResources;j++){
+                        avail[j]=avail[j]+hold[i][j];
+                        hold[i][j]=0;
+                    }
+                }
                 return 1;
             }
             else{//cancel allocation------------------------ 
+                for(int j=0;j<numResources;j++){
+                    avail[j]=avail[j]+request_vector[j];
+                }
+                for(int i = 0; i<numProcesses;i++){
+                    for(int j =0; j<numResources;j++){
+                        hold[i][j]=hold[i][j]-request_vector[j];
+                        need[i][j]=need[i][j]+request_vector[j];
+                        printf("Cancel allocation \n");
+                    }
+                }
                 //must wait
-                bankers_algorithm(pr_id,request_vector);
+                //block process until another process finishes --------------------
+                //bankers_algorithm(pr_id,request_vector);
+                return 0;
             }
 
         }
@@ -90,41 +133,85 @@ for(int i = 0; i<numProcesses;i++){
 Simulates processes running on the system.
 */
 void* process_simulator(void* pr_id){
-//thread simulating process
-//gets random request vector
-int *request_vector;
-request_vector = malloc(numResources * sizeof(int));
-request_simulator((int)pr_id,request_vector);
-//calls banker algo
-bankers_algorithm((int)pr_id,request_vector);
-//call bankers to see if allocation of request vector is safe or not
-//if not safe: process continuously calls bankers unitl allocation is safe
-//if safe: check if process can termiante and if so, free all resources hekld by process. If you cant terminate, then sleep for 3 seconds and make another request
-//only gets resource if allocation is safe
-//releases resources if it gets its needs met
+
+    //thread simulating process
+    //gets random request vector
+    int *request_vector;
+    request_vector = malloc(numResources * sizeof(int));
+    request_simulator((int)pr_id,request_vector);
+    //calls banker algo
+    bankers_algorithm((int)pr_id,request_vector); //--------------big while(bankers){} ???? or recursive bankers?
+    //call bankers to see if allocation of request vector is safe or not
+    //if not safe: process continuously calls bankers unitl allocation is safe
+    //if safe: check if process can termiante and if so, free all resources held by process. If you cant terminate, then sleep for 3 seconds and make another request
+    //only gets resource if allocation is safe
+    //releases resources if it gets its needs met
+
+    //check for remaining requests
+    bool remaining;
+    for(int j=0;j<numResources;j++){ 
+        if(need[(int)pr_id][j]!=0){//remaining requests
+            remaining=true;
+        }
+    }
+    while(remaining){
+        remaining=false;
+        request_simulator((int)pr_id,request_vector); 
+        bankers_algorithm((int)pr_id,request_vector);
+        for(int j=0;j<numResources;j++){ 
+            if(need[(int)pr_id][j]!=0){//remaining requests
+                remaining=true;
+            }
+        }
+        sleep(3);
+    }
+    // //check for remaining requests
+    // for(int j=0;j<numResources;j++){
+        
+    //     if(need[(int)pr_id][j]!=0){//remaining requests
+    //         sleep(3);
+    //         request_simulator((int)pr_id,request_vector); //----------------is this suppose to be here? call bankers again? like while still remaining repeat???
+    //         bankers_algorithm((int)pr_id,request_vector);
+    //     }
+    // }
+
+    //terminate process -------------------------free all resources here???
+    pthread_join(pr_id,NULL);
+
 }
 
 /*
 Simulates a fault occuring on the system.
 */
 void* fault_simulator(void* pr_id){
-//thread running in background removing resources with probability described in the spec
+    //thread running in background removing resources with probability described in the spec
+    //rand() % (max_number + 1 - minimum_number) + minimum_number --------------------------------how can it be 50% probability and uniform????
+    int resource = rand()%((numResources-1)+1);
+    avail[resource]-=1;
 }
 
 /*
 Checks for deadlock
 */
 void* deadlock_checker(){
-bool deadlock = false;
-//periodically run this
-//check if deadlock has occured due to resource fault
-//deadlock if Need>Avail for ALL processes for at least 1 resource type
-//process needs checked against current available resource in system
-if(deadlock){
-    printf("Deadlock will occur as process request more resources, exiting");
-    exit(1);//?????? HOW EXIT???
+    bool deadlock = false;
+    //periodically run this
+    //check if deadlock has occured due to resource fault
+    //process needs checked against current available resource in system
+    for(int i = 0; i<numProcesses;i++){
+        for(int j =0; j<numResources;j++){
+            if(need[i][j]>avail[j]){}
+            else{
+                deadlock=true;
+            }
+        }
+    }
+    if(deadlock){
+        printf("Deadlock will occur as process request more resources, exiting");
+        exit(1);//this how you exit???
+    }
 }
-}
+
 int main()
 {
 
@@ -168,33 +255,33 @@ int main()
             need[i][j]=max[i][j];
         }  
     }
-
-
     
     //create threads simulating processes (process_simulator)
     int *p_ids[numProcesses];
+
     pthread_t p_id[numProcesses];
     for (int j=0 ; j< numProcesses; j++){
         int *temp = j;
         p_ids[j] = temp;
+        printf("process %d created\n",j);
         pthread_create(&p_id[j], NULL, process_simulator,p_ids[j]);
     }
 
-    //create a thread that takes away resources from the available pool (fault_simulator)  //QUESTION 2????? 
-    // pthread_t resource;
+    //create a thread that takes away resources from the available pool (fault_simulator)  ---------------------------why does adding this? make scanf infinite????
+    // pthread_t faulty;
     // while(1){
-    //     //HOW DO YOU DO UNIFORM PROBABILITY 50% for pr_id
-    //     //pthread_create(&resource,NULL,fault_simulator,pr_id);
+    //     //pthread_create(&faulty,NULL,fault_simulator,pr_id);
     //     sleep(10);
     // }
     
 
-    // //create a thread to check for deadlock (deadlock_checker)  //QUESTION 2???????? 
+    // //create a thread to check for deadlock (deadlock_checker)  
     // pthread_t deadlock;
     //  while(1){
     //     pthread_create(&deadlock,NULL,deadlock_checker,NULL);
     //     sleep(10);
     // }
+    pthread_exit(NULL);
     return 0;
 
 }
